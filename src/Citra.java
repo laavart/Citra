99dae2a856d71706e49ad1b4375db9046b35ab36
laava <laavart11@gmail.com>
2022-02-12 20:08:10 +0530

    Commit

--- src/Citra.java
+++ src/Citra.java
@@ -7,7 +7,7 @@ public class Citra {
     static Connection connectDB(Database DB , String Hostname, int Port, String DB_User, String DB_AccessToken){
         try {
             Class.forName(DB.getDriver());
-            String Path = ;
+            String Path = "";
             return DriverManager.getConnection( Path , DB_User, DB_AccessToken);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
