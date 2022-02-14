9f181d33ab292e15288c2de8eae1f7264e0c1f40
laava <laavart11@gmail.com>
2022-02-14 20:21:31 +0530

    Commit

--- src/Database.java
+++ src/Database.java
@@ -1,15 +1,31 @@
-public enum Database {
-    MYSQL("com.mysql.jdbc.Driver"),
-    ORACLE("oracle.jdbc.driver.OracleDriver"),
-    DB2("COM.ibm.db2.jdbc.net.DB2Driver"),
-    SYBASE("com.sybase.jdbc.SybDriver");
+import java.sql.*;
 
-    final String DB_driver;
-    Database(String DB_driver) {
-        this.DB_driver = DB_driver;
+public class Database {
+
+    private Connection connection = null;
+    private Statement statement = null;
+
+    private Database(){
+        System.out.println("Connection Started !");
     }
 
-    public String getDriver(){
-        return DB_driver;
+    static Database connect(databases DB , String Path, String DB_User, String DB_AccessToken){
+        try {
+            Class.forName(DB.getDriver());
+            Database database = new Database();
+            database.connection = DriverManager.getConnection( Path , DB_User, DB_AccessToken);
+            System.out.println("Connection Established !");
+            database.statement = database.connection.createStatement();
+            return database;
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Connection Failed !");;
+            return null;
+        }
     }
+
+    public void searchInDB(String name) throws SQLException {
+        ResultSet resultSet = statement.executeQuery("");
+    }
+
 }
---
+++ src/databases.java
@@ -0,0 +1,28 @@
+public enum databases {
+    MYSQL("com.mysql.jdbc.Driver","jdbc:mysql://","/"),
+    ORACLE("oracle.jdbc.driver.OracleDriver","jdbc:oracle:thin:@",":"),
+    DB2("COM.ibm.db2.jdbc.net.DB2Driver","jdbc:db2:","/"),
+    SYBASE("com.sybase.jdbc.SybDriver","jdbc:sybase:Tds:","/");
+
+    final String DB_driver;
+    final String Path;
+    final String Link;
+
+    databases(String DB_driver, String Path, String Link) {
+        this.DB_driver = DB_driver;
+        this.Path = Path;
+        this.Link = Link;
+    }
+
+    public String getDriver(){
+        return DB_driver;
+    }
+
+    public String getPath(String Hostname, String Port, String Database){
+        return Path + Hostname + ":" + Port + Link + Database;
+    }
+
+    public String getPath(String Hostname, String Database){
+        return Path + Hostname + Link + Database;
+    }
+}
