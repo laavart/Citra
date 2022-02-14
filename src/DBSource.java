d6910d30abddf1b88558c0d8abbfc99abd7b4d20
laava <laavart11@gmail.com>
2022-02-14 21:20:36 +0530

    Commit

--- src/DBSource.java
+++ src/DBSource.java
@@ -1,28 +1,58 @@
 public enum DBSource {
-    MYSQL("com.mysql.jdbc.Driver","jdbc:mysql://","/"),
-    ORACLE("oracle.jdbc.driver.OracleDriver","jdbc:oracle:thin:@",":"),
-    DB2("COM.ibm.db2.jdbc.net.DB2Driver","jdbc:db2:","/"),
-    SYBASE("com.sybase.jdbc.SybDriver","jdbc:sybase:Tds:","/");
+    MYSQL(
+            "com.mysql.jdbc.Driver",
+            "jdbc:mysql://",
+            "/",
+            "show tables;"
+    ),
+    ORACLE(
+            "oracle.jdbc.driver.OracleDriver",
+            "jdbc:oracle:thin:@",
+            ":",
+            "select table_name, owner from all_table where owner = \""
+    ),
+    DB2(
+            "COM.ibm.db2.jdbc.net.DB2Driver",
+            "jdbc:db2:",
+            "/",
+            ""
+    ),
+    SYBASE(
+            "com.sybase.jdbc.SybDriver",
+            "jdbc:sybase:Tds:",
+            "/",
+            ""
+    );
 
-    final String DB_driver;
-    final String Path;
-    final String Link;
+    final private String DB_driver;
+    final private String Path;
+    final private String Link;
+    final private String ShowT;
 
-    DBSource(String DB_driver, String Path, String Link) {
+    DBSource(String DB_driver, String Path, String Link, String ShowT) {
         this.DB_driver = DB_driver;
         this.Path = Path;
         this.Link = Link;
+        this.ShowT = ShowT;
     }
 
     public String getDriver(){
         return DB_driver;
     }
 
-    public String getPath(String Hostname, String Port, String Database){
-        return Path + Hostname + ":" + Port + Link + Database;
+    public String getPath(String hostname, String Port, String Database){
+        return Path + hostname + ":" + Port + Link + Database;
     }
 
     public String getPath(String Hostname, String Database){
         return Path + Hostname + Link + Database;
+    }
+
+    public String getTables(String owner){
+        return ShowT + owner + "\";";
+    }
+
+    public String getTables(){
+        return ShowT;
     }
 }
--- src/Database.java
+++ src/Database.java
@@ -1,62 +1,110 @@
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
 
+    private DBSource source;
+    private String hostname;
+    private String port;
+    private String database;
+    private String user;
+    private String token;
+
     private Connection connection = null;
     private Statement statement = null;
-    private static ArrayList<DBSource> noPort;
 
+    private static ArrayList<DBSource> CheckForPort;
     static {
-        noPort = new ArrayList<>(1);
-        noPort.add(DBSource.MYSQL);
+        CheckForPort = new ArrayList<>(1);
+        CheckForPort.add(DBSource.MYSQL);
     }
 
+    private static ArrayList<DBSource> CheckByOwner;
+    static {
+        CheckByOwner = new ArrayList<>(1);
+        CheckByOwner.add(DBSource.ORACLE);
+    }
 
     private Database(){
         System.out.println("Connection Started !");
+        this.port = "";
     }
 
-    static Database connect(DBSource DB , String Hostname, String Port, String Database, String DB_User, String DB_AccessToken) throws DBInvalidException {
-        if(noPort.contains(DB)){
+    static Database connect(DBSource source , String hostname, String port, String database, String user, String token) throws DBInvalidException {
+        if(CheckForPort.contains(source)){
             throw new DBInvalidException();
         }
         else{
             try {
-                Class.forName(DB.getDriver());
-                Database database = new Database();
-                database.connection = DriverManager.getConnection(DB.getPath(Hostname, Port, Database), DB_User, DB_AccessToken);
+                Class.forName(source.getDriver());
+                Database db = new Database();
+                db.source = source;
+                db.hostname = hostname;
+                db.port = port;
+                db.database = database;
+                db.user = user;
+                db.token = token;
+
+                db.connection = DriverManager.getConnection(source.getPath(hostname, port, database), user, token);
                 System.out.println("Connection Established !");
-                database.statement = database.connection.createStatement();
-                return database;
-            } catch (ClassNotFoundException | SQLException e) {
+
+                db.statement = db.connection.createStatement();
+
+                return db;
+            }
+            catch (ClassNotFoundException | SQLException e) {
                 System.out.println("Connection Failed !");
                 return null;
             }
         }
     }
 
-    static Database connect(DBSource DB , String Hostname, String Database, String DB_User, String DB_AccessToken) throws DBInvalidException {
-        if(!noPort.contains(DB)){
+    static Database connect(DBSource source , String hostname, String database, String user, String token) throws DBInvalidException {
+        if(!CheckForPort.contains(source)){
             throw new DBInvalidException();
         }
         else {
             try {
-                Class.forName(DB.getDriver());
-                Database database = new Database();
-                database.connection = DriverManager.getConnection(DB.getPath(Hostname, Database), DB_User, DB_AccessToken);
+                Class.forName(source.getDriver());
+                Database db = new Database();
+                db.source = source;
+                db.hostname = hostname;
+                db.database = database;
+                db.user = user;
+                db.token = token;
+
+                db.connection = DriverManager.getConnection(source.getPath(hostname, database), user, token);
                 System.out.println("Connection Established !");
-                database.statement = database.connection.createStatement();
-                return database;
-            } catch (ClassNotFoundException | SQLException e) {
+
+                db.statement = db.connection.createStatement();
+
+                return db;
+            }
+            catch (ClassNotFoundException | SQLException e) {
                 System.out.println("Connection Failed !");
                 return null;
             }
         }
     }
 
-    public void searchInDB(String name) throws SQLException {
-        ResultSet resultSet = statement.executeQuery("");
+    public boolean searchTable(String name) throws SQLException {
+        {
+            ResultSet resultSet =
+                    CheckByOwner.contains(source)
+                            ? statement.executeQuery(source.getTables(database))
+                            : statement.executeQuery(source.getTables()) ;
+            while (resultSet.next()) {
+                if (resultSet.getString(1).equals(name))
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    public void createTable(String table, HashMap<String,String> fields) throws SQLException {
+        String query = "";
+        statement.executeUpdate(query);
     }
 
 }
